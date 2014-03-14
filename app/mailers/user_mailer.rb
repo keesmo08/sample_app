9fd54e1306b1a95eef1b1e23af760a3ea239e675
Matthew Keesey <mattkeesey@hotmail.com>
2014-03-14 16:46:25 -0500

    changed UserMailer default email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: "matthew.keesey@gmail.com"
+  default from: "app21166732@heroku.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
